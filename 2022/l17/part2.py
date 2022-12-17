0aeec77bec25ec064893375d267cd41640283dcf
albi-c <albert.csontos@gmail.com>
2022-12-17 11:13:34 +0100

    2022 day 17 part 2

--- 2022/l17/part2.py
+++ 2022/l17/part2.py
@@ -3,6 +3,7 @@ from ..advent import Advent, vec2
 advent = Advent(17)
 
 WIDTH = 7
+STEPS = 1_000_000_000_000
 
 ROCK_SHAPES = [
     (vec2(0, 0), vec2(1, 0), vec2(2, 0), vec2(3, 0)),
@@ -27,17 +28,22 @@ def collide_rock(rock: tuple[vec2, ...], rocks: set[tuple[int, int]]) -> bool:
 
 
 def highest_rock(rocks: set[tuple[int, int]]) -> int:
-    if len(rocks) == 0:
-        return 0
+    return max((rock[1] for rock in rocks), default=-1) + 1
 
-    return max(rock[1] for rock in rocks) + 1
+
+def get_top_pattern(rocks: set[tuple[int, int]], max_y: int) -> tuple[int, ...]:
+    return tuple(max((rock[1] - max_y for rock in rocks if rock[0] == i), default=0) for i in range(WIDTH))
 
 
 def run(moves: list[vec2]) -> int:
     rocks = set()
+    cache = {}
+
+    offset = 0
 
     move_i = 0
-    for i in range(1000000000000):
+    i = 0
+    while i < STEPS:
         y = highest_rock(rocks)
         rock = move_rock(ROCK_SHAPES[i % len(ROCK_SHAPES)], vec2(2, y + 3))
 
@@ -54,7 +60,20 @@ def run(moves: list[vec2]) -> int:
                 break
             rock = moved
 
-    return highest_rock(rocks)
+        highest = highest_rock(rocks)
+
+        if offset == 0:
+            state = (i % len(ROCK_SHAPES), move_i % len(moves)) + get_top_pattern(rocks, highest)
+            if state in cache:
+                l_h, l_i = cache[state]
+                offset = (STEPS - i) // (i - l_i) * (highest - l_h)
+                i = STEPS - (STEPS - i) % (i - l_i)
+
+            cache[state] = (highest, i)
+
+        i += 1
+
+    return highest_rock(rocks) + offset
 
 
 advent.solution(run(moves_))
