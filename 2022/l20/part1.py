9f29947617625ac17a8a1aa164ce34620671605a
albi-c <albert.csontos@gmail.com>
2022-12-21 14:13:57 +0100

    2022 day 20

--- 2022/l20/part1.py
+++ 2022/l20/part1.py
@@ -3,22 +3,13 @@ from ..advent import Advent
 advent = Advent(20)
 
 values: list[tuple[int, int]] = [(int(ln), i) if ln != "0" else (0, 0) for i, ln in enumerate(advent.read.lines())]
-moved: list[tuple[int, ...]] = values.copy()
+moved = values.copy()
 
 for val, i in values:
     i = moved.index((val, i))
-    ni = (i + val) % len(values) + (val > 0)
-    if ni == 0:
-        ni = len(moved)
+    ni = (i + val) % (len(values) - 1)
+    moved.insert(ni, moved.pop(i))
 
-    print(val)
-    print(list(p[0] for p in moved), i, ni)
-    moved.insert(ni, (val,))
-    print(list(p[0] for p in moved))
-    moved.pop(i + (ni < i))
-    print(list(p[0] for p in moved), end="\n\n")
+zero = moved.index((0, 0))
 
-offset = moved.index((0, 0))
-advent.solution(moved[(1000 + offset) % len(values)][0] +
-                moved[(2000 + offset) % len(values)][0] +
-                moved[(3000 + offset) % len(values)][0])
+advent.solution(sum(moved[((i + 1) * 1000 + zero) % len(moved)][0] for i in range(3)))
---
+++ 2022/l20/part2.py
@@ -0,0 +1,17 @@
+from ..advent import Advent
+
+advent = Advent(20)
+
+values: list[tuple[int, int]] = [(int(ln) * 811589153, i) if ln != "0" else (0, 0)
+                                 for i, ln in enumerate(advent.read.lines())]
+moved = values.copy()
+
+for _ in range(10):
+    for val, i in values:
+        i = moved.index((val, i))
+        ni = (i + val) % (len(values) - 1)
+        moved.insert(ni, moved.pop(i))
+
+zero = moved.index((0, 0))
+
+advent.solution(sum(moved[((i + 1) * 1000 + zero) % len(moved)][0] for i in range(3)))
