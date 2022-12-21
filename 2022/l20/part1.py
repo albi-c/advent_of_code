cc703da99e1f29f7510bbe41d9fd5c6d673fcd5b
albi-c <albert.csontos@gmail.com>
2022-12-21 11:07:42 +0100

    2022 day 20 works on test input

--- 2022/l20/part1.py
+++ 2022/l20/part1.py
@@ -2,4 +2,23 @@ from ..advent import Advent
 
 advent = Advent(20)
 
-values: list[tuple[int, int]] = [(i, int(ln)) for i, ln in enumerate(advent.read.lines())]
+values: list[tuple[int, int]] = [(int(ln), i) if ln != "0" else (0, 0) for i, ln in enumerate(advent.read.lines())]
+moved: list[tuple[int, ...]] = values.copy()
+
+for val, i in values:
+    i = moved.index((val, i))
+    ni = (i + val) % len(values) + (val > 0)
+    if ni == 0:
+        ni = len(moved)
+
+    print(val)
+    print(list(p[0] for p in moved), i, ni)
+    moved.insert(ni, (val,))
+    print(list(p[0] for p in moved))
+    moved.pop(i + (ni < i))
+    print(list(p[0] for p in moved), end="\n\n")
+
+offset = moved.index((0, 0))
+advent.solution(moved[(1000 + offset) % len(values)][0] +
+                moved[(2000 + offset) % len(values)][0] +
+                moved[(3000 + offset) % len(values)][0])
