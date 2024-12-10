d5c86b27f1d398d13b41cfb0ac0e41cbaec7f42e
albi-c <albert.csontos@gmail.com>
2024-12-10 19:35:34 +0100

    2024 day 4 with new library functions

--- 2024/l4/solution.py
+++ 2024/l4/solution.py
@@ -12,40 +12,12 @@ DIRECTIONS = (
     ivec2(-1, -1)
 )
 
-grid = Grid(Advent().read())
+grid = Grid.parse(Advent().read())
 
-count = 0
-for x in range(grid.width):
-    for y in range(grid.height):
-        pos = ivec2(x, y)
-        if grid[pos] != "X":
-            continue
-        for d in DIRECTIONS:
-            if not grid.inside(pos + 3 * d):
-                continue
-            fail = False
-            for i, ch in enumerate("MAS", start=1):
-                if grid[pos + i * d] != ch:
-                    fail = True
-                    break
-            if not fail:
-                count += 1
-print(count)
+print(sum(1 for _ in grid.sequence_search("XMAS", True)))
 
-count = 0
-for x in range(grid.width):
-    for y in range(grid.height):
-        pos = ivec2(x, y)
-        if grid[pos] != "A":
-            continue
-        fail = False
-        for o in (ivec2(1, 1), ivec2(-1, 1)):
-            if not grid.inside(pos + o) or not grid.inside(pos - o):
-                fail = True
-                break
-            if {grid[pos + o], grid[pos - o]} != set("MS"):
-                fail = True
-                break
-        if not fail:
-            count += 1
-print(count)
+print(sum(sum(1 for _ in grid.subgrid_search(Grid([[a, None, b],
+                                                   [None, "A", None],
+                                                   [c, None, d]]),
+                                             lambda x, y: y is None or x == y))
+          for a, b, c, d in ("MMSS", "MSMS", "SSMM", "SMSM")))
