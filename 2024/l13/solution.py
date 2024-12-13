f575c5187c08948a99442b6a95131e6710793206
albi-c <albert.csontos@gmail.com>
2024-12-13 16:13:08 +0100

    2024 day 13

--- 2024/l13/solution.py
+++ 2024/l13/solution.py
@@ -1,25 +1,31 @@
 from advent import Advent, ivec2
+import z3
 
 
 input_data: list[list[ivec2]] = [[ivec2(*(int(x[2:]) for x in ln.split(": ")[1].split(", "))) for ln in block]
                                  for block in Advent().read.blocks().lines()()]
 
 
-def find_best(machine: list[ivec2]) -> int:
-    best = 1 << 30
+def solve(machine: list[ivec2], add: int) -> int:
     a, b, target = machine
-    for i in range(101):
-        pa = a * i
-        if pa.x > target.x or pa.y > target.y:
-            continue
-        rem = target - pa
-        if rem.x % b.x != 0 or rem.y % b.y != 0:
-            continue
-        rem //= b
-        if rem.x != rem.y:
-            continue
-        best = min(best, 3 * i + rem.x)
-    return best if best != 1 << 30 else 0
+    solver = z3.Optimize()
+
+    na = z3.Int("na")
+    solver.add(na >= 0)
+    nb = z3.Int("nb")
+    solver.add(nb >= 0)
+
+    solver.add(na * a.x + nb * b.x == target.x + add)
+    solver.add(na * a.y + nb * b.y == target.y + add)
+
+    cost = z3.Int("cost")
+    solver.add(cost == 3 * na + nb)
+    solver.minimize(cost)
+
+    if solver.check() == z3.sat:
+        return solver.model().eval(cost).as_long()
+    return 0
 
 
-print(sum(map(find_best, input_data)))
+print(sum(solve(machine, 0) for machine in input_data))
+print(sum(solve(machine, 10000000000000) for machine in input_data))
