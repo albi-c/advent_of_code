c9733219eac53a2ee80724f412e522ce64e34b7a
albi-c <albert.csontos@gmail.com>
2024-12-18 18:31:56 +0100

    2024 day 18 proper solution

--- 2024/l18/solution.py
+++ 2024/l18/solution.py
@@ -1,4 +1,4 @@
-from advent import Advent, Grid, ivec2
+from advent import Advent, Grid, ivec2, pathfind_max_cost, dijkstra, find_threshold
 from collections import defaultdict
 
 coordinates: list[ivec2] = [ivec2(*ln) for ln in Advent().read.lines().split(",").map(int)()]
@@ -6,44 +6,17 @@ size = 71
 target = ivec2(size, size) - 1
 
 
-def pathfind(grid: Grid[bool], start: ivec2, end: ivec2) -> int:
-    dist = defaultdict(lambda: 1 << 30)
-    q = set()
-    for pos, val in grid.enumerate():
-        if not val:
-            q.add(pos)
-    dist[start] = 0
-    while q:
-        u = min(q, key=lambda pos: dist[pos])
-        q.remove(u)
-        for v in grid.neighbors(u):
-            if grid[v] or v not in q:
-                continue
-            alt = dist[u] + 1
-            if alt < dist[v]:
-                dist[v] = alt
-                q.add(v)
-    return dist[end]
+def pathfind(grid: Grid[bool]) -> int:
+    return dijkstra(ivec2(0, 0), lambda p: (n for n in grid.neighbors(p) if not grid[n]), lambda u, v: 1)[0][target]
 
 
-def solve1(n):
+def solve(n: int) -> int:
     grid = Grid.of(False, size, size)
     for coord in coordinates[:n]:
         grid[coord] = True
-    print(pathfind(grid, ivec2(0, 0), target))
+    return pathfind(grid)
 
 
-def solve2():
-    grid = Grid.of(False, size, size)
-    for i, coord in enumerate(coordinates):
-        grid[coord] = True
-        # found with binary search by hand
-        if i < 2990:
-            continue
-        if pathfind(grid, ivec2(0, 0), target) == 1 << 30:
-            print(f"{coord.x},{coord.y}")
-            break
-
-
-solve1(1024)
-solve2()
+print(solve(1024))
+result = coordinates[find_threshold(solve, pathfind_max_cost, 1024, len(coordinates) - 1) - 1]
+print(f"{result.x},{result.y}")
